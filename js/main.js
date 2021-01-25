b860ae8c11291512549df085669acc27f455801c
DenisLemeshko <lemeshko.denis@yandex.ru>
2021-01-25 15:53:38 +0300

    final

--- js/main.js
+++ js/main.js
@@ -4,7 +4,29 @@ $(function(){
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		arrows: false,
-		dots: true
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 769,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				}
+			}
+		]
+	});
+
+
+	$('.menu__btn').on('click', function(){
+		
+		$('.menu__list').toggleClass('menu__list--active')
 	});
 
 });
